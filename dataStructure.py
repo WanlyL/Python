c02284824ddc5f08469694d5192a894d5eda493a
WanlyL <138504923+WanlyL@users.noreply.github.com>
2023-10-13 16:54:09 -0400

    Python Data Structure

--- dataStructure.py
+++ dataStructure.py
@@ -145,4 +145,4 @@ def friend_date(a, b):
     if set(a[2]) & set(b[2]):
         return True 
     else:
-        return False                                                                                                                
+        return False g                                                                                                               
