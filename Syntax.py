4d8f0d2fc2086a86b65364caa087449c2fe5fdc7
WanlyL <138504923+WanlyL@users.noreply.github.com>
2023-10-16 10:42:41 -0400

    Added OOP

--- Syntax.py
+++ Syntax.py
@@ -79,3 +79,45 @@ def sum_nums(nums):
 
 print("sum_nums returned", sum_nums([1, 2, 3, 4]))
 
+
+#OOP Python 
+
+class SerialGenerator:
+
+def __init__(self, start=0):
+    self.start = self.next = start 
+
+def __repr__ (self):
+    return f"<SerialGenerator start={self.start} next={self.next}>"
+
+def generate(self):
+    self.next += 1 
+    return self.next - 1 
+
+def reset(self):
+    self.next = self.start 
+
+class WordFinder:
+
+def _init_(self, path):
+    dict_file = open(path)
+
+    self.words = self.parse(dict_file)
+
+    print(f"{len(self.words)} words read")
+
+def parse(self, dict_file):
+    return [w.strip() for w in dict_file]
+
+def random(self):
+
+    return random.choice(self.words)  
+
+
+class SpeicalWordFinder(WordFinder):
+
+    def parse(self, dict_file):
+
+        return [w.strip() for w in dict_file
+        if w.strip() and not w.startswith("#")]  
+
